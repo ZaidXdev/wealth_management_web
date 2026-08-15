cf1d777753ae0c805b61ad780c29b3608bf32166
Zaid Swaileh <121728782+ZaidXdev@users.noreply.github.com>
2026-08-15 20:27:38 +0300

    add api key and add headers

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3664240591" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "52764020" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
