abba3b3f0dbd7f39d30f140195cbc2ee453625ee
Zaid Swaileh <121728782+ZaidXdev@users.noreply.github.com>
2026-09-05 17:20:42 +0300

    Price history redesign & AI button

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2451338987" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1452048024" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
